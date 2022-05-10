a4c189039941ffd5713339f67db6c734ec7883b7
Диана Минкова <dianaminkova@yahoo.com>
2022-05-10 16:49:02 +0300

    Update word_to_pdf.py

--- word_to_pdf.py
+++ word_to_pdf.py
@@ -1,16 +1,16 @@
 from comtypes.client import CreateObject
 import os
 
-folder = 'C:\Test_PDF'
-wdToPDF = CreateObject("Word.Application")
+folder = '...'  #input address in your folder
+wdToPDF = CreateObject('Word.Application')
 wdFormatPDF = 17
 files = os.listdir(folder)
-word_files = [f for f in files if f.endswith((".doc", ".docx"))]
+word_files = [f for f in files if f.endswith(('.doc', '.docx'))]
 for word_file in word_files:
     word_path = os.path.join(folder, word_file)
     pdf_path = word_path
     if pdf_path[-3:] != 'pdf':
-        pdf_path = pdf_path + ".pdf"
+        pdf_path = pdf_path + '.pdf'
 
     if os.path.exists(pdf_path):
         os.remove(pdf_path)
